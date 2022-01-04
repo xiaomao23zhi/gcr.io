e77ed9586e73c5a4a2df7433d8dccc8f1f4401eb
xiaomao23zhi <gokasama@icloud.com>
2022-01-04 14:27:36 +0800

    Update sparkmagic.sh

--- notebook/sparkmagic.sh
+++ notebook/sparkmagic.sh
@@ -2,7 +2,8 @@
 ################################################################################
 # Set sparkmagic config
 ################################################################################
-LIVY_CONF=~/.sparkmagic/config.json
+livyConf=~/.sparkmagic/config.json
+livyServer=${LIVY_SERVER:-"localhost:8998"}
 
 # generate sparkmagic config
 function sparkMagicConf() {
@@ -10,18 +11,17 @@ function sparkMagicConf() {
     
 import json
 import os
-with open('~/${LIVY_CONF}.template', 'r') as template_json:
+with open('${LIVY_CONF}.template', 'r') as template_json:
     data = json.loads(template_json.read())
 data['kernel_python_credentials']['url']="$livyServer"
 data['kernel_scala_credentials']['url']="$livyServer"
 data['kernel_r_credentials']['url']="$livyServer"
 
-data['session_configs']={
-    'proxyUser': ${JUPYTERHUB_USER}
-}
+data['session_configs']['proxyUser']="$JUPYTERHUB_USER"
+
 print(json.dumps(data))
 =EOM
 }
 
 # Main
-sparkMagicConf > ${LIVY_CONF}
+sparkMagicConf > $livyConf
